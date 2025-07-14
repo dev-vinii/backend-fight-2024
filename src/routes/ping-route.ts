79c21ee7015067bb5b6a830cd8bb60406faf6e37
Vinicius Santos <vinicius.santos@amazonasinovare.com.br>
2025-07-14 16:58:14 -0300

    refactor: simplify ping route implementation

--- src/routes/ping-route.ts
+++ src/routes/ping-route.ts
@@ -1,8 +1,7 @@
 import { FastifyInstance } from "fastify";
-import { PingController } from "../controllers/ping-controller";
 
-export default async function pin(fastify: FastifyInstance) {
-  const pingController = new PingController();
-
-  fastify.get("/", pingController.ping.bind(pingController));
+export default async function ping(fastify: FastifyInstance) {
+  fastify.get("/", async (request, reply) => {
+    reply.send({ message: "pong" });
+  });
 }
