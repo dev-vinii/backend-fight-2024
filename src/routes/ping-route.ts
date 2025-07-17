a8795fc3d454d7318a7e7ac36ee420e5ec3c05f8
Vinicius Luna <viniciusluna@yahoo.com>
2025-07-16 22:49:44 -0300

    chore(app): set up swagger in project

--- src/routes/ping-route.ts
+++ src/routes/ping-route.ts
@@ -1,7 +1,24 @@
 import { FastifyInstance } from "fastify";
 
 export default async function ping(fastify: FastifyInstance) {
-  fastify.get("/", async (request, reply) => {
-    reply.send({ message: "pong" });
-  });
+  fastify.get(
+    "/ping",
+    {
+      schema: {
+        description: "Health check endpoint",
+        tags: ["Health"],
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      reply.send({ message: "pong" });
+    }
+  );
 }
