32489d977079a86a1d0dbd620393155e422d6c58
Vinicius Luna <viniciusluna@yahoo.com>
2025-07-20 19:57:28 -0300

    feat(statement): enhance statement generation and update route schema

--- src/controllers/statement-controller.ts
+++ src/controllers/statement-controller.ts
@@ -1,6 +1,6 @@
 import { db } from "@/db/database";
 import { clients, transactions } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function generateStatement(
@@ -9,38 +9,39 @@ export async function generateStatement(
   }>,
   reply: FastifyReply
 ) {
-  const client = await db.query.clients.findFirst({
-    where: eq(clients.id, request.params.id),
-  });
+  try {
+    const clientId = request.params.id;
 
-  if (!client) {
-    return reply.status(404).send({ error: "Client not found" });
+    const client = await db.query.clients.findFirst({
+      where: eq(clients.id, clientId),
+    });
+
+    if (!client) {
+      return reply.status(404).send({ error: "Client not found" });
+    }
+
+    const clientTransactions = await db.query.transactions.findMany({
+      where: eq(transactions.clientId, clientId),
+      orderBy: desc(transactions.realizedAt),
+    });
+
+    const { balance, limit } = client;
+
+    return reply.status(200).send({
+      balance: {
+        total: balance,
+        date: new Date().toISOString(),
+        limit,
+      },
+      last_transactions: clientTransactions?.map((transaction) => ({
+        value: transaction.value,
+        type: transaction.type,
+        description: transaction.description,
+        realizedAt: transaction.realizedAt.toISOString(),
+      })),
+    });
+  } catch (error) {
+    console.error("Error generating statement:", error);
+    return reply.status(500).send({ error: "Internal server error" });
   }
-
-  const clientTransactions = await db.query.transactions.findMany({
-    where: eq(transactions.clientId, request.params.id),
-    orderBy: (t) => t.realizedAt,
-  });
 }
-
-// {
-//   "saldo": {
-//     "total": -9098,
-//     "data_extrato": "2024-01-17T02:34:41.217753Z",
-//     "limite": 100000
-//   },
-//   "ultimas_transacoes": [
-//     {
-//       "valor": 10,
-//       "tipo": "c",
-//       "descricao": "descricao",
-//       "realizada_em": "2024-01-17T02:34:38.543030Z"
-//     },
-//     {
-//       "valor": 90000,
-//       "tipo": "d",
-//       "descricao": "descricao",
-//       "realizada_em": "2024-01-17T02:34:38.543030Z"
-//     }
-//   ]
-// }
--- src/routes/statement-route.ts
+++ src/routes/statement-route.ts
@@ -1,47 +1,57 @@
-// import { FastifyInstance } from "fastify";
+import { generateStatement } from "@/controllers/statement-controller";
+import { FastifyInstance } from "fastify";
 
-// export default async function statementRoutes(fastify: FastifyInstance) {
-//   const clientParamsSchema = {
-//     type: "object",
-//     required: ["clientId"],
-//     properties: {
-//       clientId: { type: "string" },
-//     },
-//   };
+export default async function statementRoutes(fastify: FastifyInstance) {
+  const clientParamsSchema = {
+    type: "object",
+    required: ["id"],
+    properties: {
+      id: { type: "string" },
+    },
+  };
 
-//   fastify.get(
-//     "/clients/:clientId/statement",
-//     {
-//       schema: {
-//         description: "Get the statement of a client",
-//         tags: ["Statements"],
-//         params: clientParamsSchema,
-//         response: {
-//           200: {
-//             type: "object",
-//             properties: {
-//               transactions: {
-//                 type: "array",
-//                 items: {
-//                   type: "object",
-//                   properties: {
-//                     value: { type: "number" },
-//                     type: { type: "string", enum: ["c", "d"] },
-//                     description: { type: "string" },
-//                     date: { type: "string", format: "date-time" },
-//                   },
-//                 },
-//               },
-//               balance: { type: "number" },
-//             },
-//           },
-//           404: {
-//             type: "object",
-//             properties: {
-//               error: { type: "string" },
-//             },
-//           },
-//         },
-//       },
-//     },
-//   }
+  fastify.get(
+    "/clients/:id/statement",
+    {
+      schema: {
+        description: "Get the statement of a client",
+        tags: ["Statements"],
+        params: clientParamsSchema,
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              balance: {
+                type: "object",
+                properties: {
+                  total: { type: "number" },
+                  date: { type: "string", format: "date-time" },
+                  limit: { type: "number" },
+                },
+              },
+              last_transactions: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    value: { type: "number" },
+                    type: { type: "string", enum: ["c", "d"] },
+                    description: { type: "string" },
+                    realizedAt: { type: "string", format: "date-time" },
+                  },
+                },
+              },
+            },
+          },
+          404: {
+            type: "object",
+            properties: {
+              error: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    generateStatement
+  );
+}
